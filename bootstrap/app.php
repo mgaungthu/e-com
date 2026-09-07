2f01a08a2d8a2a636b9c417b7d157f401de7c62f
Aung Thu <mr.mgaungthu@gmail.com>
2026-09-07 22:23:57 +0700

    add apiPrefix

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,7 +7,7 @@ use App\Http\Middleware\EnsureEmailIsVerified;
 use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
-    ->withRouting(web: __DIR__.'/../routes/web.php', api: __DIR__.'/../routes/api.php', commands: __DIR__.'/../routes/console.php', health: '/up')
+    ->withRouting(web: __DIR__.'/../routes/web.php', api: __DIR__.'/../routes/api.php', commands: __DIR__.'/../routes/console.php', health: '/up', apiPrefix: '')
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->alias([
             'verified.api' => EnsureEmailIsVerified::class,
